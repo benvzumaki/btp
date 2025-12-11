236a47e317eeac501fd04d48b0bde362db9e7bb9
banny tri pamungkas <banny10114501@gmail.com>
2025-12-11 08:17:42 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         tl.fromTo(".text-foot", { x: -10, opacity: 0 }, { x: 0, opacity: 1 })
     }, [])
     return (
-        <footer className="overflow-hidden fixed flex items-center gap-2 font-my-font text-xs left-[-155px] lg:left-[-85px] bottom-40 text-my-white-dark dark:text-my-black-light -rotate-90">
+        <footer className="overflow-hidden fixed flex items-center gap-2 font-my-font text-xs left-[-180px] sm:left-[-160px] md:left-[-120px] lg:left-[-85px] bottom-40 text-my-white-dark dark:text-my-black-light -rotate-90">
             <span className="line-foot inline-block relative h-[2px] w-14 rounded-full bg-my-white-dark dark:bg-my-black-light"><span className="absolute right-[-1px] top-[-5px] inline-block h-3 w-3 rounded-full bg-my-white-dark dark:bg-my-black-light"></span></span>
             <p className='text-foot'>Hak Cipta &copy; {year} - BTP - Dirancang oleh Banny Tri Pamungkas</p>
         </footer>
